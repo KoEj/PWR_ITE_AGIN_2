0b8540a40301f37e75cb4a28bb117fd8c6817624
Pawel Niedziolka <pawel.niedziolka@pl.whoohoo.com>
2023-01-23 11:20:07 +0100

    random solution best/worst/avg

--- Solution.cpp
+++ Solution.cpp
@@ -8,17 +8,41 @@ extern int number_of_trucks;
 extern int number_of_cities;
 extern int capacity;
 extern int N;
+extern int N_random;
 double antWorst;
 double best_distance;
 double worst_distance;
 double average_distance;
 
 void randomSolution(std::string filename, bool loaderPrint, bool distanceMatrixPrint) {
+	double sumBestDistance = 0.0;
+	double best_random_distance = std::numeric_limits<double>::max();
+	double worst_random_distance = std::numeric_limits<double>::min();
 	Loader(filename, loaderPrint);
 	createDistanceMatrix(number_of_cities, distanceMatrixPrint);
-	std::vector<std::vector<int>> truckRoutesId = random(number_of_trucks, capacity, number_of_cities);
-	std::vector<double> truckDistances = calculateResultDstVect(0, truckRoutesId);
-	calculateResult("Random", truckDistances);
+
+	for (int i = 0; i < N_random; i++) {
+
+		average_distance = 0;
+
+		std::vector<std::vector<int>> truckRoutesId = random(number_of_trucks, capacity, number_of_cities);
+		std::vector<double> truckDistances = calculateResultDstVect(0, truckRoutesId);
+		double result = calculateResult("Random", truckDistances);
+		sumBestDistance += result;
+
+		if (result < best_random_distance) {
+			best_random_distance = result;
+		}
+		if (result > worst_random_distance) {
+			worst_random_distance = result;
+		}
+	}
+
+	std::cout << "Random best: " << best_random_distance << std::endl;
+	std::cout << "Random worst: " << worst_random_distance << std::endl;
+	std::cout << "Random avg : " << sumBestDistance / N_random << std::endl;
+
+
 }
 
 void greedySolution(std::string filename, bool loaderPrint, bool distanceMatrixPrint) {
